17c013ea63fcdcf3ad25e8422c0866abd9b28b1f
corpglory-dev <47055832+corpglory-dev@users.noreply.github.com>
2020-01-14 20:08:09 +0300

    "id" and "title" props (#2)

--- dist/vue-gauge.umd.js
+++ dist/vue-gauge.umd.js
@@ -2561,12 +2561,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"55bcbba2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Gauge.vue?vue&type=template&id=0fedd9d8&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _vm._m(0)}
-var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('h3',[_vm._v("Gauge")]),_c('div',{attrs:{"id":"gauge"}})])}]
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"55bcbba2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Gauge.vue?vue&type=template&id=f92795f0&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('h5',{staticClass:"gauge-title"},[_vm._v(_vm._s(_vm.title))]),_c('div',{attrs:{"id":_vm.id}})])}
+var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/Gauge.vue?vue&type=template&id=0fedd9d8&
+// CONCATENATED MODULE: ./src/components/Gauge.vue?vue&type=template&id=f92795f0&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.concat.js
 var es_array_concat = __webpack_require__("99af");
@@ -15529,7 +15529,7 @@ function (_Vue) {
     value: function mounted() {
       var _this3 = this;
 
-      this.svg = src_select('#gauge').append('svg').attr('width', this.width).attr('height', this.height);
+      this.svg = src_select("#".concat(this.id)).append('svg').attr('width', this.width).attr('height', this.height);
       this.gaugeCenter = "translate(".concat(this.width / 2, ",").concat(this.height - 10, ")");
       var arc = src_arc().innerRadius(50).outerRadius(80).padAngle(0);
       var pie = src_pie().startAngle(-1 * Math.PI / 2).endAngle(Math.PI / 2);
@@ -15563,6 +15563,10 @@ function (_Vue) {
 }(external_commonjs_vue_commonjs2_vue_root_Vue_default.a);
 
 __decorate([Prop({
+  required: true
+})], Gaugevue_type_script_lang_ts_Gauge.prototype, "id", void 0);
+
+__decorate([Prop({
   required: true,
   default: function _default() {
     return DEFAULT_COLORS;
@@ -15580,6 +15584,10 @@ __decorate([Prop({
 __decorate([Prop({
   required: true
 })], Gaugevue_type_script_lang_ts_Gauge.prototype, "maxValue", void 0);
+
+__decorate([Prop({
+  required: false
+})], Gaugevue_type_script_lang_ts_Gauge.prototype, "title", void 0);
 
 __decorate([Prop({
   required: false,
